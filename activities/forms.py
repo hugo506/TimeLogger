2b4dbf0ff83d7d61331cd2f9bbc049dad5a57352
Prakhar Srivastav <prakhar1989@gmail.com>
2013-08-10 22:25:50 +0530

    Added validation error for start and end date

--- activities/forms.py
+++ activities/forms.py
@@ -9,3 +9,12 @@ class ActivityForm(forms.ModelForm):
 class ReportsDateForm(forms.Form):
     start_date = forms.DateField()
     end_date = forms.DateField()
+
+    def clean(self):
+        cleaned_data = super(ReportsDateForm, self).clean()
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
+
+        if start_date > end_date:
+            raise forms.ValidationError("Start date has to be earlier than end date!")
+
