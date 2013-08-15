46987c284d85de000cf4304402aa491603b7723f
Prakhar Srivastav <prakhar1989@gmail.com>
2013-08-16 00:33:47 +0530

    Added fabric for deployment

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,23 @@
 from fabric.api import local
 
 def prepare_deploy():
-    local("echo DEPLOYING APP TO PRODUCTION...")
+    local("echo ------------------------")
+    local("echo DEPLOYING APP TO PRODUCTION")
     local("git add . && git commit")
     local("git push -u origin master")
+    local("echo APP PUSHED TO PRODUCTION")
+    local("echo ------------------------")
+
+def commit(msg):
+    local("git add . && git commit -am %s" % msg)
+
+def deploy():
+    prepare_deploy()
+    code_dir = "/home/captain/Code/django_apps/TimeLogger"
+    with cd(code_dir):
+        run("git pull")
+
+def restart_service():
+    run("sudo supervisorctl restart django_app")
+
+
