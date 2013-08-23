c26881fc665f89b74e51863bcb9bb4ab14d943e8
Prakhar Srivastav <prakhar1989@gmail.com>
2013-08-23 12:41:31 +0530

    added pagination to leaves views

--- leaves/views.py
+++ leaves/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from leaves.models import Leave
 from leaves.forms import LeaveForm
 from django.contrib import messages
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def index(request):
-    leaves = Leave.objects.filter(author = request.user)
+    results = Leave.objects.filter(author = request.user)
+    paginator = Paginator(results, 8)
+    page = request.GET.get('page')
     if request.method == "POST":
         form = LeaveForm(request.POST)
         if form.is_valid():
@@ -16,5 +19,11 @@ def index(request):
             messages.add_message(request, messages.SUCCESS, "Leave added successfully")
         else:
             form = LeaveForm()
+    try:
+        leaves = paginator.page(page)
+    except PageNotAnInteger:
+        leaves = paginator.page(1)
+    except EmptyPage:
+        leaves = paginator.page(paginator.num_pages)
     context = { 'leaves' : leaves }
     return render(request, "leaves/index.html", context)
